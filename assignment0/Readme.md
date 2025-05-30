941bcc8b62f6b950ab89cdb54ab409bd20f35410
OrangeEgg1937 <48792791+OrangeEgg1937@users.noreply.github.com>
2025-05-30 18:13:33 +0800

    impl: Finished the AS0 Transformation

---
+++ assignment0/Readme.md
@@ -0,0 +1,23 @@
+# GAMES101 - Assignment 0
+
+> I modified the original CMakeLists.txt to work with my environment without using the GAMES101 VM.
+
+## Lecture 03 Transformation
+
+https://sites.cs.ucsb.edu/~lingqi/teaching/resources/GAMES101_Lecture_03.pdf
+
+## Matrix Transformations
+
+https://www.geogebra.org/m/sqG26hQj
+
+## Result
+
+```
+Final point is P'=(1.70711,4.12132)
+```
+
+### Validation
+
+With using the geogebra link above, we can verify the program output:
+
+![Validation](validation.png)
--- assignment0/main.cpp
+++ assignment0/main.cpp
@@ -3,6 +3,33 @@
 #include<Eigen/Dense>
 #include<iostream>
 
+#define PI 3.14159265
+
+// Define the rotation matrix R(a)
+Eigen::Matrix3f rotation_matrix(float degress) {
+    
+    Eigen::Matrix3f _m;
+    float _radians = degress * M_PI / 180;
+
+    _m << std::cos(_radians), -std::sin(_radians), 0,
+          std::sin(_radians),  std::cos(_radians), 0,
+                0           ,          0         , 1;
+    
+    return _m;
+}
+
+// Define the translation matrix T(tx, ty)
+Eigen::Matrix3f translation_matrix(float tx, float ty) {
+    
+    Eigen::Matrix3f _m;
+
+    _m << 1, 0, tx,
+          0, 1, ty,
+          0, 0,  1;
+    
+    return _m;
+}
+
 int main(){
 
     // Basic Example of cpp
@@ -36,20 +63,47 @@ int main(){
     Eigen::Matrix3f i,j;
     i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
     j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
+    
     // matrix output
     std::cout << "Example of output \n";
     std::cout << i << std::endl;
+    
     // matrix add i + j
+    std::cout << "Example of matrix add (i+j) \n";
+    std::cout << i + j << std::endl;
+    
     // matrix scalar multiply i * 2.0
+    std::cout << "Example of matrix scalar multiply (i*2.0) \n";
+    std::cout << i * 2.0f << std::endl;
+    
     // matrix multiply i * j
+    std::cout << "Example of matrix multiply (i*j) \n";
+    std::cout << i * j << std::endl;
+
     // matrix multiply vector i * v
+    std::cout << "Example of matrix multiply vector (i*v) \n";
+    std::cout << i * v << std::endl;
 
-
+    std::cout << "== START OF AS0 ==\n";
     /* 
     * PA 0
+    * 1. 给定一个点 P=(2,1), 将该点绕原点先逆时针旋转45◦
+    * 2. 再平移(1,2), 计算出变换后点的坐标（要求用齐次坐标进行计算）。
+    * 
+    * 1. Given a point P=(2,1), rotate the point counterclockwise by 45 degrees around the origin,
+    * 2. then translate it by (1,2). Calculate the coordinates of the transformed point (using homogeneous coordinates).
     */
-    // TO DO: Define point P
-    // TO DO: Define rotation matrix M
-    // TO DO: M * P
+
+    // Define the point P in homogeneous coordinates
+    Eigen::Vector3f P(2.0f, 1.0f, 1.0f); // (x, y, 1)
+    
+    /*
+    *  ALWAYS APPLY IT FROM RIGHT TO LEFT 
+    */
+    P = translation_matrix(1, 2) * rotation_matrix(45) * P;
+
+    std::cout << "Result:\n" << P << "\n\n" << "Final point is P'=(" << P[0] << "," << P[1] << ")\n";
+
+    std::cout << "== END OF AS0 ==";
     return 0;
 }
